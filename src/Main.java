4a87f278d06538c029e03ca313dec7643ff2e2d0
Aaron <aquelat1409@g.educaand.es>
2024-11-05 10:42:58 +0100

    Creada rama nueva-funcionalidad y agregados cambios en Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("¡Hola mundo!, esta es una nueva funcionalidad");
+        System.out.println("¿Has visto que nueva funcionalidad mas guapa?");
     }
 }
